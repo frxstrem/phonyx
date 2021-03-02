03d31a2aa563d9293e0fd8ba0d11905f7d1a4d9c
Fredrik Østrem <frx.apps@gmail.com>
2021-03-02 22:29:40 +0100

    frontend: Add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
-import { Navbar } from 'react-bootstrap';
-import './App.css';
+import { Container, Navbar } from "react-bootstrap";
+import { Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand>Phonyx</Navbar.Brand>
+        <Navbar.Brand as={Link} to={"/"}>
+          Phonyx
+        </Navbar.Brand>
       </Navbar>
+
+      <Container>
+        <Switch>
+          <Route exact path="/">
+            <div>Home</div>
+          </Route>
+
+          {/* Fallback route */}
+          <Route>
+            <div>404 Page Not Found</div>
+          </Route>
+        </Switch>
+      </Container>
     </div>
   );
 }
